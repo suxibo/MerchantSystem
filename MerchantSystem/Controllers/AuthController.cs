1d5e8ab4d3787c7ba397cf00dcb0e479cd928f14
苏锡波 <vsxibo@163.com>
2019-02-04 23:06:42 +0800

    Update /auth/login page.

--- MerchantSystem/Controllers/AuthController.cs
+++ MerchantSystem/Controllers/AuthController.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using MerchantSystem.Filters;
 
 namespace MerchantSystem.Controllers
 {
-    [ValidateLogin]
-    public class HomeController : Controller
+    public class AuthController : Controller
     {
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Login()
         {
             return View();
         }
